9c8971905555848d379b3b2ff3146032f6fd44f1
Isabella Bernal <bellabernalcarvajal871@gmail.com>
2026-01-08 17:46:29 -0500

    first commit

---
+++ mamoryJar/src/main/java/com/project/mamoryJar/MamoryJarApplication.java
@@ -0,0 +1,13 @@
+package com.project.mamoryJar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamoryJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamoryJarApplication.class, args);
+	}
+
+}
